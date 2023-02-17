5ece10e3fa941496ad158cb7bfe467b6fa8d1f35
SaadWaheed10 <saad46697@gmail.com>
2023-02-17 12:30:12 +0500

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,63 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-function App() {
+/** import all components */
+import Username from "./components/Username";
+import PageNotFound from "./components/PageNotFound";
+import Password from "./components/Password";
+import Profile from "./components/Profile";
+import Recovery from "./components/Recovery";
+import Register from "./components/Register";
+import Reset from "./components/Reset";
+
+/** auth middleware */
+import { AuthorizeUser, ProtectRoute } from "./middleware/auth";
+
+/**root routes */
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Username></Username>,
+  },
+  {
+    path: "/register",
+    element: <Register></Register>,
+  },
+  {
+    path: "/*",
+    element: <PageNotFound></PageNotFound>,
+  },
+  {
+    path: "/password",
+    element: (
+      <ProtectRoute>
+        <Password />
+      </ProtectRoute>
+    ),
+  },
+  {
+    path: "/profile",
+    element: (
+      <AuthorizeUser>
+        <Profile />
+      </AuthorizeUser>
+    ),
+  },
+  {
+    path: "/recovery",
+    element: <Recovery></Recovery>,
+  },
+  {
+    path: "/reset",
+    element: <Reset></Reset>,
+  },
+]);
+
+export default function App() {
   return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
   );
 }
-
-export default App;
-
